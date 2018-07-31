ee66f235d4b20bb2a44816f1be0808bb72d96692
fli136 <fli136@gmail.com>
2018-07-31 13:56:06 -0700

    lab2pt3

--- lab2/fli_lab2_part3/fli_lab2_part3/main.c
+++ lab2/fli_lab2_part3/fli_lab2_part3/main.c
@@ -1,6 +1,6 @@
 /*	Partner(s) Name & E-mail: Jasmine Kwong jkwon045@ucr.edu
  *	Lab Section: 21
- *	Assignment: Lab 2  Exercise 2 
+ *	Assignment: Lab 2  Exercise 3
  *	Exercise Description: [optional - include for your own benefit]
  *	
  *	I acknowledge all content contained herein, excluding template or example
@@ -23,36 +23,42 @@ int main(void)
 	DDRC = 0xFF; PORTC = 0x00;
 	
 	unsigned char lights = 0x00;
-	unsigned char fuel;
+	unsigned char fuel = 0x00;
+	unsigned char seatbelt = 0x00;
 	
     /* Replace with your application code */
     while (1) 
     {
 		lights = 0x00;
 		fuel = 0x0F & PINA;
-		if (fuel > 1) {
+		if (fuel > 0) {
 			lights = SetBit(lights, 5, 1);
 		}
-		if (fuel > 3) {
+		if (fuel > 2) {
 			lights = SetBit(lights, 4, 1);
 		}
-		if (fuel > 5) {
+		if (fuel > 4) {
 			lights = SetBit(lights, 3, 1);
 		}
-		if (fuel > 7) {
+		if (fuel > 6) {
 			lights = SetBit(lights, 2, 1);
 		}
-		if (fuel > 10) {
+		if (fuel > 9) {
 			lights = SetBit(lights, 1, 1);
 		}
-		if (fuel > 13) {
+		if (fuel > 12) {
 			lights = SetBit(lights, 0, 1);
 		}
 		if (fuel < 5) {
 			lights = SetBit(lights, 6, 1);
 		}
 		
+		seatbelt = PINA & 0x70;
+		seatbelt = (seatbelt == 0x30) ? 1 : 0;
+		lights = SetBit(lights, 7, seatbelt);
 		PORTC = lights;
     }
 }
+
+
 
