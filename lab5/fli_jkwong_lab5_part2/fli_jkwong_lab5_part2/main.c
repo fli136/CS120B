d22185f70b02f9042df087a0e9bfe583f5f1790e
fli136 <fli136@gmail.com>
2018-08-07 12:45:24 -0700

    lab 5 pt 2

---
+++ lab5/fli_jkwong_lab5_part2/fli_jkwong_lab5_part2/main.c
@@ -0,0 +1,153 @@
+/*
+ * fli_jkwong_lab5_part1.c
+ *
+ * Created: 8/6/2018 1:44:52 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+unsigned char light;
+unsigned char button;
+
+enum states { init, light1, light2, light3, press, release, reset } state = -1;
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+void LT_tick() {
+	switch(state) {
+		case init: 
+			state = light1;
+			break;
+		case light1:
+			state = (button) ? press : light2;
+			break;
+		case light2:
+			state = (button) ? press : light3;
+			break;
+		case light3:
+			state = (button) ? press : light1;
+			break;
+		case press:
+			state = (button) ? press : release;
+			break;
+		case release:
+			state = (button) ? reset : release;
+			break;
+		case reset:
+			state = (button) ? reset : light1;
+			break;
+		default:
+			state = init;
+			break;
+		
+	}
+	
+	switch(state) {
+		case init:
+			light = 0x00;
+			break;
+		case light1:
+			light = 0x01;
+			break;
+		case light2:
+			light = 0x02;
+			break;
+		case light3:
+			light = 0x04;
+			break;
+		case press:
+			break;
+		case release:
+			break;
+		case reset:
+			break;
+		default:
+			break;
+	
+	}
+	PORTB = light;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+int main()
+{
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF; // Set port B to output
+	PORTB = 0x00; // Init port B to 0s
+	TimerSet(300);
+	TimerOn();
+	light = 0x00;
+	button = 0x00;
+	while(1) {
+		// User code (i.e. synchSM calls)
+		button = ~PINA & 0x01;
+		LT_tick();	// Toggle PORTB; Temporary, bad programming style
+
+		while (!TimerFlag);	// Wait 1 sec
+		TimerFlag = 0;
+		// Note: For the above a better style would use a synchSM with TickSM()
+		// This example just illustrates the use of the ISR and flag
+	}
+	return 0;
+}
+
+
