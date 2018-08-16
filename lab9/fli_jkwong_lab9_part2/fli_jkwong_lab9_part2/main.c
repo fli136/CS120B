7136507543aab5ce0e7c7d29f667c632fc17c9b6
fli136 <fli136@gmail.com>
2018-08-16 14:41:00 -0700

    9/1

---
+++ lab9/fli_jkwong_lab9_part2/fli_jkwong_lab9_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * fli_jkwong_lab9_part2.c
+ *
+ * Created: 8/16/2018 2:40:19 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
